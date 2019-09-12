4574fdc641d9fba75980c02e6c1f9553413e5f53
Piyush Mehta <me@piyushmehta.com>
2019-09-12 19:23:45 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const csv = require('csv-parser');
 const fs = require('fs');
-const csvToJson = require('convert-csv-to-json');
+var fileOutputName = require('./data.json');
 
 var app = express();
 // configure body parser
@@ -13,18 +12,6 @@ var port = process.env.PORT || 8080; // set our port
 
 // create our router
 var router = express.Router();
-var fileInputName = 'S.csv'; 
-var fileOutputName = 'data.json';
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
-
-fs.createReadStream('S.csv')
-  .pipe(csv())
-  .on('data', row => {
-    console.log(row);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  });
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -35,7 +22,8 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
+  res.header('Content-Type', 'application/json');
+  res.send(fileOutputName);
 });
 
 // REGISTER OUR ROUTES -------------------------------
