f719947cb3b3b4aa4b395a89d591a0d793c6fcb3
Piyush Mehta <me@piyushmehta.com>
2019-09-12 15:16:33 +0530

    fin

---
+++ src/views/Dashboard/components/OngoingTasks/onGoingTasks.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100%'
+  },
+  content: {
+    alignItems: 'center',
+    display: 'flex'
+  },
+  title: {
+    fontWeight: 700
+  },
+  avatar: {
+    backgroundColor: theme.palette.success.main,
+    height: 56,
+    width: 56
+  },
+  icon: {
+    height: 32,
+    width: 32
+  },
+  difference: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    alignItems: 'center'
+  },
+  differenceIcon: {
+    color: theme.palette.success.dark
+  },
+  differenceValue: {
+    color: theme.palette.success.dark,
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const OnGoingTasks = props => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardContent>
+        <Grid
+          container
+          justify="space-between"
+        >
+          <Grid item>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+              variant="h1"
+            >
+              On Going Tasks{' '}
+            </Typography>
+            <Typography variant="h3">""</Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+OnGoingTasks.propTypes = {
+  className: PropTypes.string
+};
+
+export default OnGoingTasks;
--- src/views/UserList/UserList.js
+++ src/views/UserList/UserList.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+import OnGoingTasks from 'views/Dashboard/components/OngoingTasks/onGoingTasks';
+import FullFilledTasks from 'views/Dashboard/components/OngoingTasks/FulFilledTasks';
+import PendingTasks from 'views/Dashboard/components/OngoingTasks/PendingTasks';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,7 +17,42 @@ const useStyles = makeStyles(theme => ({
 const UserList = () => {
   const classes = useStyles();
 
-  return <div className={classes.root} />;
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={4}
+          sm={4}
+          xl={4}
+          xs={12}
+        >
+          <FullFilledTasks />
+        </Grid>
+        <Grid
+          item
+          lg={4}
+          sm={4}
+          xl={4}
+          xs={12}
+        >
+          <OnGoingTasks />
+        </Grid>
+        <Grid
+          item
+          lg={4}
+          sm={4}
+          xl={4}
+          xs={12}
+        >
+          <PendingTasks />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default UserList;
