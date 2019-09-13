47b15b0826ea5c07abea1e98e21888eb631a5265
Piyush Mehta <me@piyushmehta.com>
2019-09-13 07:54:56 +0530

    api fix

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 const fs = require('fs');
 var fileOutputName = require('./data.json');
 var cors = require('cors');
-
+var axios = require('axios');
 var app = express();
 // configure body parser
 app.use(cors());
@@ -27,7 +27,38 @@ router.get('/', function(req, res) {
   res.header('Content-Type', 'application/json');
   res.send(fileOutputName);
 });
+let apiKaData;
+let godownLat = 12.967195;
+let godownlon = 80.127865;
+router.get('/distance-cal', function(req, res) {
+  let address;
 
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.lat},${req.query.long}&key=AIzaSyCfQ0EjpxAe8P7UBFtQ74kxZIRZsEyL4G4`
+    )
+    .then(function(response) {
+      // handle success
+      apiKaData = response.data;
+      address = apiKaData.results[0].formatted_address.split(' ').join('+');
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function() {
+      console.log(address);
+      axios
+        .get(
+          `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Plot+No.60,+8th+St,+Shankar+Nagar,+Pammal,+Chennai&destinations=${address}&key=AIzaSyCfQ0EjpxAe8P7UBFtQ74kxZIRZsEyL4G4`
+        )
+        .then(function(response) {
+          // handle success
+          res.header('Content-Type', 'application/json');
+          res.send(response.data);
+        });
+    });
+});
 // REGISTER OUR ROUTES -------------------------------
 app.use('/', router);
 
